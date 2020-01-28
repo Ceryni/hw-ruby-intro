31d585e971b44750f86535053b6ebd8e21938ffc
Ceryni <matthew.fitzpatrick6012@gmail.com>
2020-01-28 12:48:56 -0600

    Started working on it a little.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,48 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each{|x| total += x}
+  total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  sorted = arr.sort
+  if sorted.length == 0
+    sum = 0
+  elsif sorted.length == 1
+    sum = sorted[0]
+  else
+    sum = sorted[-1] + sorted[-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  result = false
+  if arr.length != 0
+    arr.combination(2) do |c|
+      if c.sum == n
+        result = true
+      end
+    end
+  end
+  result
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # A, E, I, O, U
+  if /[aeiou]/i.match(s[0])
+    false
+  else
+    true
+  end
 end
 
 def binary_multiple_of_4? s
